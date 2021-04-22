55988ac0e1b96a2f07f285fd5f492066beb1347b
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-04-22 13:37:51 -0700

    added front end and working with back end

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 //parse incoming JSON data
 app.use(express.json());
+// use css/js/image files in the public folder; makes it static so no endpoints needed for each file
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let filteredResults = animalsArray;
@@ -98,6 +100,22 @@ app.get('/api/animals/:id', (req, res) => {
     else {
         res.send(404);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.post('/api/animals', (req, res) => {
